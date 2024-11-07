63409c3db158be01e76e32fc7c958fcd91f41115
yeasinarafat51 <aloondokar65@gmail.com>
2024-11-08 05:03:24 +0600

    problem

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ require('dotenv').config();
 // App setup
 const app = express();
 const PORT = process.env.PORT || 8000;
-app.use(cors());
+const corsOptions = {
+    origin: [
+      'http://localhost:5173',
+     
+      
+     
+    ],
+    credentials: true,
+   
+  }
+  app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.static('uploads'));
 
